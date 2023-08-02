ed0728683732952ecb5704154c9bf998db85851c
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-02 21:12:04 +0530

    Create subtract.c

---
+++ subtract.c
@@ -0,0 +1,13 @@
+// Aim: Code for subtraction of three numbers
+#include <stdio.h>
+
+int main() {
+    int x, y, z, output;
+    x = 5;
+    y = 4;
+    z = 3;
+    output = x - y - z;
+    printf("The negation of three numbers is %d", output);
+
+    return 0;
+}
