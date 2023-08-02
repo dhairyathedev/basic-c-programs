30d8fd09cd512ec96bff2062ad73a7632906ef35
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-02 21:12:53 +0530

    Create divide.c

---
+++ divide.c
@@ -0,0 +1,12 @@
+// Aim: Code for division of two numbers
+#include <stdio.h>
+
+int main() {
+    int x, y, output;
+    x = 6;
+    y = 2;
+    output = x / y;
+    printf("The division of two numbers is %d", output);
+
+    return 0;
+}
