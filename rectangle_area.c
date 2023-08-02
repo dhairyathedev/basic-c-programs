ab7c12f75858f45da883f36d219408b9f41f6056
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-02 21:19:01 +0530

    Create rectangle_area.c

---
+++ rectangle_area.c
@@ -0,0 +1,14 @@
+// Aim: Area of Rectangle
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter the length of a rectangle: ");
+    scanf("%d", &x);
+    
+    printf("Enter the breadth of a rectangle: ");
+    scanf("%d", &y);
+    
+    printf("Area of rectangle is %d sq units", x*y);
+    return 0;
+}
