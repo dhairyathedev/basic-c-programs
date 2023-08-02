628d93050854940b5affed7e4457784e2cb35d3b
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-02 21:11:49 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,13 @@
+// Aim: Code for addition of three numbers
+#include <stdio.h>
+
+int main() {
+    int x, y, z, output;
+    x = 5;
+    y = 4;
+    z = 3;
+    output = x + y + z;
+    printf("The sum of three numbers is %d", output);
+
+    return 0;
+}
