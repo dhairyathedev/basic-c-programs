6520c6ae8f0947f0700f0e836558e20550ee9566
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-02 21:16:59 +0530

    Create square_area.c

---
+++ square_area.c
@@ -0,0 +1,10 @@
+// Aim: Area of Square
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Enter the side of a square: ");
+    scanf("%d", &x);
+    printf("Area of square is %d sq units", x*x);
+    return 0;
+}
